771a9c144ecade8e97e927e333268e9e8fb67fd8
Pedro Martin <pataruco@gmail.com>
2018-07-11 21:32:46 +0100

    //WIP

---
+++ paths/date-id-file-id.js
@@ -0,0 +1,53 @@
+const AWS = require('aws-sdk');
+const POD_BUCKET_NAME = 'peter-of-the-day';
+
+const s3 = new AWS.S3();
+
+const params = {
+  Bucket: `${POD_BUCKET_NAME}/production/manifest`,
+  Key: 'manifest.json',
+};
+const dateregex = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/gm;
+
+const getMAnifest = () => {
+  return new Promise((resolve, reject) => {
+    s3.getObject(params, (error, data) => {
+      if (error) {
+        return reject(console.error(error));
+      }
+
+      const succcess = () => {
+        return JSON.parse(data.Body.toString('utf-8'));
+      };
+
+      resolve(succcess());
+    });
+  });
+};
+
+const getDate = (manifest, dateId) => {
+  return manifest.dates.filter(date => date.date === dateId);
+};
+
+const getFile = (date, fileId) => {
+  const index = parseInt(fileId, 10) - 1;
+  return date.files[index].url;
+};
+
+exports.handler = async event => {
+  const { dateId, fileId } = event.pathParameters;
+  const dateId = event.pathParameters.dateId;
+  const manifest = await getMAnifest();
+  const date = getDate(manifest, dateId);
+  const isDateString = dateregex.test(dateId);
+  const fileId = event.pathParameters.fileId;
+  const file = getFile(date, fileId);
+  const isdateIdANumber = Number.isInteger(parseInt(fileId, 10));
+
+  return {
+    isBase64Encoded: false,
+    statusCode: 200,
+    headers: {},
+    body: JSON.stringify(event.pathParameters),
+  };
+};
