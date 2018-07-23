139e0f4815dffd5eafd9233bb00277e70ff1fd3f
Pedro Martin <pataruco@gmail.com>
2018-07-23 16:09:28 +0100

    create a base path

--- paths/base.js
+++ paths/base.js
@@ -1,13 +1,12 @@
-const description = {
-  description: "An endpoint to collect pictures of Peter Martin Blanco",
-  schema: "https://github.com/pataruco/pod-api/blob/master/config/swagger.yaml"
-};
-
-exports.handler = () => {
+exports.handler = async () => {
   return {
     isBase64Encoded: false,
     statusCode: 200,
     headers: {},
-    body: JSON.stringify(description)
+    body: JSON.stringify({
+      description: "An endpoint to collect pictures of Peter Martin Blanco",
+      schema:
+        "https://github.com/pataruco/pod-api/blob/master/config/swagger.yaml"
+    })
   };
 };
