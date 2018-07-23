b0e4140519d4cd2f2a75f794faab1495fdd6a97a
Pedro Martin <pataruco@gmail.com>
2018-07-23 10:56:29 +0100

    Return an image on /random and /date/fileId

--- paths/random.js
+++ paths/random.js
@@ -24,6 +24,26 @@ const getMAnifest = () => {
   });
 };
 
+const getPicture = fileName => {
+  const pictureParams = {
+    Bucket: `${POD_BUCKET_NAME}`,
+    Key: fileName
+  };
+  return new Promise((resolve, reject) => {
+    s3.getObject(pictureParams, (error, data) => {
+      if (error) {
+        return reject(console.error(error));
+      }
+
+      const succcess = () => {
+        return data.Body.toString("base64");
+      };
+
+      resolve(succcess());
+    });
+  });
+};
+
 const getRandomNumber = maxNumber => {
   return Math.floor(Math.random() * maxNumber) + 1;
 };
@@ -37,10 +57,23 @@ exports.handler = async () => {
   const randomFileNumber = getRandomNumber(maxFileNumber);
   const file = POD_URL + randomDate.files[randomFileNumber - 1].url;
 
+  if (file) {
+    return {
+      isBase64Encoded: true,
+      statusCode: 200,
+      headers: {
+        "Content-type": "image/jpeg"
+      },
+      body: await getPicture(fileName)
+    };
+  }
+
   return {
-    isBase64Encoded: false,
-    statusCode: 200,
+    isBase64Encoded: true,
+    statusCode: 400,
     headers: {},
-    body: JSON.stringify(file)
+    body: JSON.stringify({
+      message: "file not found"
+    })
   };
 };
